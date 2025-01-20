dbd3c43aa1e464d50c0ac81c0f76b09fa055cffe
Ulla Edelbo Raaschou <ulee@demant.com>
2025-01-20 19:55:54 +0100

    secondtryDayTwo

---
+++ AdventCode1/SecondTryCodeDayTwo.cs
@@ -0,0 +1,91 @@
+﻿
+namespace AdventCode2024
+{
+    public class SecondTryCodeDayTwo
+    {
+        // All level diff >=1 &&  level diff <= 3
+        // All levels must be asc or desc
+        // Max one level must be ignored
+        public static int CounterOfdiffsBetweenOneAndThreeIncl(int[] reportArray)
+        {
+            int counter = 0;
+
+            for (int i = 1; i < reportArray.Length; i++)
+            {
+                if (!(Math.Abs(reportArray[i] - reportArray[i - 1]) >= 1 && Math.Abs(reportArray[i] - reportArray[i - 1]) <= 3))
+                {
+                    counter++;
+                    break;
+                }
+
+            }
+            return counter;
+        }
+
+        public static int CounterOfDiffsNotfollowingAscDescPattern(int[] reportArray)
+        {
+            int ascCounter = 0;
+            int descCounter = 0;
+            int notAscDescCounter = 0;
+
+            for (int i = 1; i < reportArray.Length; i++)
+            {
+                if (reportArray[i] > reportArray[i - 1])
+                {
+                    descCounter++;
+                }
+                if (reportArray[i] < reportArray[i - 1])
+                {
+                    ascCounter++;
+                }
+                if (reportArray[i] == reportArray[i - 1])
+                {
+                    notAscDescCounter++;
+                }
+            }
+            if ((Math.Abs(ascCounter - descCounter) > 1) || (notAscDescCounter > 1))
+            {
+                int counterOfDominatingAscOrDescDiffs = Math.Max(ascCounter, descCounter);
+                int counterOfDiifsNotFollowingAscDescPattern = (reportArray.Length - 1) - counterOfDominatingAscOrDescDiffs;
+                return counterOfDiifsNotFollowingAscDescPattern;
+            }
+            return -1;
+
+        }
+
+        //public static int  OneDiffErrorCanBeAcccepted(int[] reportArray)
+        //{
+
+        //    int counterOfDiffsNOTBetweenOneAndThreeIncl = 0;
+
+
+        //    for (int i = 1; i < reportArray.Length; i++)
+        //    {
+        //        if (!(Math.Abs(reportArray[i] - reportArray[i - 1]) >= 1 && Math.Abs(reportArray[i] - reportArray[i - 1]) <= 3))
+        //        {
+
+        //            counterOfDiffsNOTBetweenOneAndThreeIncl++;  
+        //            break;
+        //        }
+
+        //    }
+
+        //    switch (counterOfDiffsNOTBetweenOneAndThreeIncl)
+        //    {
+        //        case 0:
+        //            // Handle when counterOfDiffsNOTBetweenOneAndThreeIncl is 0
+        //            break;
+        //        case 1:
+        //            // Handle when counterOfDiffsNOTBetweenOneAndThreeIncl is 1
+        //            break;
+        //        case 2:
+        //            // Handle when counterOfDiffsNOTBetweenOneAndThreeIncl is 2
+        //            break;
+        //        default:
+        //            // Handle when counterOfDiffsNOTBetweenOneAndThreeIncl is greater than 2
+        //            break;
+        //    }
+        //    return counterOfDiffsNOTBetweenOneAndThreeIncl;
+
+    }
+}
---
+++ TestProjectAdvent/SecondTryCodeDayTwoTests.cs
@@ -0,0 +1,49 @@
+﻿using AdventCode2024;
+
+namespace TestProjectAdvent
+{
+    [TestClass]
+    public class SecondTryCodeDayTwoTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 7, 6, 4, 2, 1 }, 0)]
+        [DataRow(new int[] { 1, 2, 7, 8, 9 }, 1)]
+        public void Test_CounterOfdiffsBetweenOneAndThreeIncl(int[] reportArray, int expectedCounter)
+        {
+            //Act
+            int counter = SecondTryCodeDayTwo.CounterOfdiffsBetweenOneAndThreeIncl(reportArray);
+
+            //Assert
+            Assert.AreEqual( expectedCounter, counter);
+        }
+
+
+
+        [TestMethod]
+        [DataRow(new int[] { 7, 6, 4, 2, 1 }, 0)]
+        [DataRow(new int[] { 1, 2, 7, 8, 9 }, 0)]
+        [DataRow(new int[] { 8, 6, 1, 4, 1 }, 1)]
+        public void Test_CounterOfDiffsNotfollowingAscDescPattern(int[] reportArray, int expectedCounter)
+        {
+            //Act
+            int counter = SecondTryCodeDayTwo.CounterOfDiffsNotfollowingAscDescPattern(reportArray);
+
+            //Assert
+            Assert.AreEqual(expectedCounter, counter);
+
+        }
+
+        //[TestMethod]
+        //[DataRow(new int[] { 7, 6, 4, 2, 1 }, 0)]
+        //public void OneLevelErrorCanBeAccepted(int[] reportArray,  int counterOfDiffsNOTBetweenOneAndThreeIncl)
+        //{
+        //    //Act
+        //    int result = SecondTryCodeDayTwo.OneDiffErrorCanBeAcccepted(reportArray);
+
+        //    //Assert
+            
+        //    Assert.AreEqual(counterOfDiffsNOTBetweenOneAndThreeIncl, result);
+           
+        //}
+    }
+}
